0c927d2dcdb0560f682e169c316be8b2478a5b83
Lucas Hourahine <luca5h1905@gmail.com>
2020-05-10 13:38:19 -0400

    name input

---
+++ iotesting1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string input;
+
+  cout << "Type some characters: ";
+  cin >> input;
+  cout << input;
+  cout << "\n";
+  return 0;
+}

